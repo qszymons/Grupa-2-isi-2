77b0daadf70fccbf081174d3eb210681331d0159
mlooe <wiktorkrasinski2@gmail.com>
2026-03-11 19:03:10 +0100

    [BE][1.1] Add user email validation

--- Base-App/app/src/core/domain/user.py
+++ Base-App/app/src/core/domain/user.py
@@ -1,13 +1,23 @@
 """Model containing user-related models"""
 
 from enum import Enum
-from pydantic import BaseModel, ConfigDict, UUID1
+from email_validator import validate_email, EmailNotValidError
+from pydantic import BaseModel, ConfigDict, UUID1, field_validator
 
 
 class UserIn(BaseModel):
     """An input user model."""
     email: str
     password: str
+
+    @field_validator("email")
+    @classmethod
+    def validate_email_address(cls, val: str) -> str:
+        try:
+            valid = validate_email(val)
+            return valid.email
+        except EmailNotValidError as e:
+            raise ValueError(f"Invalid email at: {e}")
 
 
 class User(UserIn):
